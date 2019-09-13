c0e15081208d9b1eb6454673dda8f922686e0961
cobyz <cobyz@192.168.1.27>
2019-09-12 20:50:17 -0700

    check error checking methods

--- src/cs310zimmerman/CS310Zimmerman.java
+++ src/cs310zimmerman/CS310Zimmerman.java
@@ -17,14 +17,16 @@ public class CS310Zimmerman {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        final String INPUT_FILENAME = "input/assn1input.txt";
         
         //testing license validity
-        Broker broker = new Broker("abc12345", "Coby", "Zimmerman", "123-5555", 80.0);
+        /*Broker broker = new Broker("abc12345", "Coby", "Zimmerman", "123-5555", 80.0);
         Broker brokerTwo = new Broker("123abcde", "Tate", "Zimmerman", "146-1111", 90.0);
         Broker brokerThree = new Broker("abc43cd4", "Cayden", "Zim", "dept3", 100.0);
         
         System.out.println(broker.toString());
         System.out.println(brokerTwo.toString());
+        */
         
         /*if (brokerThree.isValidLicense()) {
             System.out.println("License is Valid");
@@ -35,12 +37,55 @@ public class CS310Zimmerman {
         */
         
         //testing dept validity
-        if (broker.isValidDept()) {
+        /*if (broker.isValidDept()) {
             System.out.println("Dept is Valid");
         }
         else {
             System.out.println("Dept is invalid");
         }
+        */
+        
+        /*
+        StockTrade trader = new StockTrade("XXWW", 20.5, 12, "abc12345", true);
+        System.out.println(trader.toString());
+        //testing stock symbol validity
+        if (trader.isValidStockSymbol()) {
+            System.out.println("Stock Symbol is valid");
+        }
+        else {
+            System.out.println("Stock symbol is invalid");
+        }
+        */
+        
+        /*
+        StockTrade traderTwo = new StockTrade("WWX", 1000.00, 13, "abc12345", false);
+        //testing price valiidty
+        if (traderTwo.isValidPrice()) {
+            System.out.println("Price is valid");
+        }
+        else {
+            System.out.println("Price is invalid");
+        }
+        */
+        
+        /*
+        StockTrade traderThree = new StockTrade("WXWX", 1000.00, 100000, "abc12345", true);
+        //testing validity of number of shares
+        if (traderThree.isValidWholeShares()) {
+            System.out.println("Valid number of shares");
+        }
+        else {
+            System.out.println("Invalid number of shares");
+        }
+        */
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
