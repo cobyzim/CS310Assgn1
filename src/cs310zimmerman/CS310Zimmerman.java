121d8464fc2ebd256a3e5ce65bbbfd1acd3f4710
cobyz <cobyz@192.168.1.27>
2019-09-12 22:34:06 -0700

    update broker class 2

--- src/cs310zimmerman/CS310Zimmerman.java
+++ src/cs310zimmerman/CS310Zimmerman.java
@@ -19,8 +19,78 @@ public class CS310Zimmerman {
     public static void main(String[] args) {
         final String INPUT_FILENAME = "input/assn1input.txt";
         
+        System.out.println("Running Test 1a:");
+        System.out.println("Broker Object 1:");
+        Broker broker = new Broker("abc12345", "Coby", "Zimmerman", "123-542", 80.0);
+        System.out.println(broker.toString());
+        System.out.println();
+        
+        System.out.println("Running Test 1b:");
+        System.out.println("Stock Trade Object 1:");
+        StockTrade trader = new StockTrade("XXWW", 20.5, 12, "abc12345", true);
+        System.out.println(trader.toString());
+        System.out.println();
+        
+        System.out.println("Running Test 2a:");
+        System.out.println("Broker Object 2:");
+        Broker brokerTwo = new Broker("abc12345", "Coby", "Zimmerman", "123-542", 80.0);
+        System.out.println(brokerTwo.toString());
+        if (brokerTwo.equals(broker)) {
+            System.out.println("The two broker objects ARE equal.");
+        }
+        else {
+            System.out.println("The two broker objects ARE NOT equal.");
+        }
+        System.out.println();
+        
+        System.out.println("Running Test 2b:");
+        System.out.println("Broker Object 2:");
+        Broker brokerThree = new Broker("abc67891", "Cayden", "Zim", "dept3", 100.0);
+        System.out.println(brokerThree.toString());
+        if (brokerThree.equals(broker)) {
+            System.out.println("The two broker objects ARE equal.");
+        }
+        else {
+            System.out.println("The two broker objects ARE NOT equal.");
+        }
+        System.out.println();
+        
+        System.out.println("Running Test 2c:");
+        System.out.println("Stock Trade Object 2:");
+        StockTrade traderTwo = new StockTrade("XXWW", 20.5, 12, "abc12345", true);
+        System.out.println(traderTwo.toString());
+        if (traderTwo.equals(trader)) {
+            System.out.println("The two stock trade objects ARE equal.");
+        }
+        else {
+            System.out.println("The two stock trade objects ARE NOT equal.");
+        }
+        System.out.println();
+        
+        System.out.println("Running Test 2d:");
+        System.out.println("Stock Trade Object 3:");
+        StockTrade traderThree = new StockTrade("WXYZ", 20.5, 12, "abc12345", false);
+        System.out.println(traderThree.toString());
+        if (traderThree.equals(trader)) {
+            System.out.println("The two stock trade objects ARE equal.");
+        }
+        else {
+            System.out.println("The two stock trade objects ARE NOT equal.");
+        }
+        System.out.println();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
         //testing license validity
-        /*Broker broker = new Broker("abc12345", "Coby", "Zimmerman", "123-5555", 80.0);
+        Broker broker = new Broker("abc12345", "Coby", "Zimmerman", "123-542", 80.0);
         Broker brokerTwo = new Broker("123abcde", "Tate", "Zimmerman", "146-1111", 90.0);
         Broker brokerThree = new Broker("abc43cd4", "Cayden", "Zim", "dept3", 100.0);
         
@@ -35,9 +105,9 @@ public class CS310Zimmerman {
             System.out.println("License is not Valid");
         }
         */
-        
+        /*
         //testing dept validity
-        /*if (broker.isValidDept()) {
+        if (brokerTwo.isValidDept()) {
             System.out.println("Dept is Valid");
         }
         else {
