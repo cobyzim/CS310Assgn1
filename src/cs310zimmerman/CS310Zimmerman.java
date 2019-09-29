abcc74044fd4fae7a641463ccf7022f99c4f815f
cobyz <cobyz@192.168.1.27>
2019-09-29 15:35:07 -0700

    updated main to call addBroker and display correct output

--- src/cs310zimmerman/CS310Zimmerman.java
+++ src/cs310zimmerman/CS310Zimmerman.java
@@ -8,9 +8,9 @@
  */
 package cs310zimmerman;
 
-
 import java.util.Scanner;
 import java.io.*;
+
 /**
  *
  * @author Coby Zimmerman
@@ -21,179 +21,82 @@ public class CS310Zimmerman {
     /**
      * @param args the command line arguments
      * @throws java.io.FileNotFoundException
-     * 
+     *
      * Main method that instantiates broker and stock trade objects, comparing
-     * them to one another based on their contents, and determining whether
-     * or not the information within them is valid.
+     * them to one another based on their contents, and determining whether or
+     * not the information within them is valid.
      */
+    static BrokerLogImpl brokerLogImpl = new BrokerLogImpl();
     public static void main(String[] args) throws FileNotFoundException {
-        final String INPUT_FILENAME = "input/assn1input.txt";
-        
+    
+        processFile();
         /*
-        System.out.println("Running Test 1a:");
-        System.out.println("Broker Object 1:");
-        Broker broker = new Broker("abc12345", "Coby", "Zimmerman", "123-542", 
-                80.0);
-        System.out.println(broker.toString());
-        System.out.println();
-        
-        System.out.println("Running Test 1b:");
-        System.out.println("Stock Trade Object 1:");
-        StockTrade trader = new StockTrade("XXWW", 20.5, 12, "abc12345", true);
-        System.out.println(trader.toString());
-        System.out.println();
-        
-        System.out.println("Running Test 2a:");
-        System.out.println("Broker Object 2:");
-        Broker brokerTwo = new Broker("abc12345", "Coby", "Zimmerman", 
-                "123-542", 80.0);
-        System.out.println(brokerTwo.toString());
-        if (brokerTwo.equals(broker)) {
-            System.out.println("The two broker objects ARE equal.");
+            StockTrade filledStockTrade = new StockTrade(arrOfStr);
+
+            if (!filledStockTrade.isValidStockSymbol()) {
+                System.out.println(filledStockTrade.toString());
+                System.out.println("ERROR: Invalid stock symbol format:"
+                        + " " + filledStockTrade.getStockSymbol());
+                System.out.println();
+                hasStockErrors = true;
+            }
+            if (!filledStockTrade.isValidPrice()) {
+                System.out.println(filledStockTrade.toString());
+                System.out.println("ERROR: Invalid stock price: "
+                        + filledStockTrade.getPricePerShare());
+                System.out.println();
+                hasStockErrors = true;
+            }
+            if (!filledStockTrade.isValidWholeShares()) {
+                System.out.println(filledStockTrade.toString());
+                System.out.println("ERROR: Invalid number of shares: "
+                        + filledStockTrade.getWholeShares());
+                System.out.println();
+                hasStockErrors = true;
+            }
         }
-        else {
-            System.out.println("The two broker objects ARE NOT equal.");
+    }
+
+    System.out.println (brokerLogImpl.getBrokerLog());
         }
-        System.out.println();
-        
-        System.out.println("Running Test 2b:");
-        System.out.println("Broker Object 2:");
-        Broker brokerThree = new Broker("abc67891", "Cayden", "Zim", "dept3",
-                100.0);
-        System.out.println(brokerThree.toString());
-        if (brokerThree.equals(broker)) {
-            System.out.println("The two broker objects ARE equal.");
-        }
-        else {
-            System.out.println("The two broker objects ARE NOT equal.");
-        }
-        System.out.println();
-        
-        System.out.println("Running Test 2c:");
-        System.out.println("Stock Trade Object 2:");
-        StockTrade traderTwo = new StockTrade("XXWW", 20.5, 12, "abc12345",
-                true);
-        System.out.println(traderTwo.toString());
-        if (traderTwo.equals(trader)) {
-            System.out.println("The two stock trade objects ARE equal.");
-        }
-        else {
-            System.out.println("The two stock trade objects ARE NOT equal.");
-        }
-        System.out.println();
-        
-        System.out.println("Running Test 2d:");
-        System.out.println("Stock Trade Object 3:");
-        StockTrade traderThree = new StockTrade("WXYZ", 20.5, 12, "abc12345",
-                false);
-        System.out.println(traderThree.toString());
-        if (traderThree.equals(trader)) {
-            System.out.println("The two stock trade objects ARE equal.");
-        }
-        else {
-            System.out.println("The two stock trade objects ARE NOT equal.");
-        }
-        System.out.println();
-        */
-        
-        System.out.println("Running Test 3:");
+           
+    }
+    */
+    }
+    public static void processFile() throws FileNotFoundException {
+        final String INPUT_FILENAME = "input/assn1input.txt";
         
         try {
            File text = new File(INPUT_FILENAME);
            Scanner scnr = new Scanner(text);
-           BrokerLogImpl brokerLogImpl = new BrokerLogImpl();
+          
            
-           while(scnr.hasNextLine()) {
-               boolean hasBrokerErrors = false;
-               boolean hasStockErrors = false;
-               String str = scnr.nextLine();
-               String[] arrOfStr = str.split(",", 7);
-               if (arrOfStr[0].equals("BROKER")) {
-                   if (arrOfStr[1].equals("ADD")) {
-                       System.out.println("ADDING BROKER");
-                       Broker filledBroker = new Broker(arrOfStr);
-                       
-                       boolean brokerAdded = brokerLogImpl.addBroker(filledBroker);
-                       
-                       if (!filledBroker.isValidLicense()) {
-                       System.out.println(filledBroker.toString());
-                       System.out.println("ERROR: Invalid broker license number"
-                               + " format: " + filledBroker.getBrokerLicense());
-                       System.out.println();
-                       hasBrokerErrors = true;
-                   }
-                   if (!filledBroker.isValidDept()) {
-                       System.out.println(filledBroker.toString());
-                       System.out.println("ERROR: Invalid department number"
-                               + " format: " + filledBroker.getDept());
-                       System.out.println();
-                       hasBrokerErrors = true;
-                   }
-                   
-                   if (!hasBrokerErrors) {
-                       displayBrokerAttributes(filledBroker);
-                   }
-                   
-                   }
-                   if (arrOfStr[1].equals("DEL")) {
-                       System.out.println("DELETING BROKER");
-                       boolean brokerRemoved = brokerLogImpl.removeBroker(arrOfStr[2]);
-                   }
-                   
-                   /*Broker emptyBroker = new Broker();
-                   Broker filledBroker = setBrokerAttributes(emptyBroker,
-                           arrOfStr);
-                   */
-                   
-                   
-                   
-                   
-                   
-               }
-               if (arrOfStr[0].equals("TRADE")) {
-                   if (arrOfStr[1].equals("BUY")) {
-                       System.out.println("BUYING STOCK");
-                   }
-                   if (arrOfStr[1].equals("SELL")) {
-                       System.out.println("SELLING STOCK");
-                   }
-                   
-                   /*
-                   StockTrade emptyStockTrade = new StockTrade();
-                   StockTrade filledStockTrade = setStockTradeAttributes(
-                           emptyStockTrade, arrOfStr);
-                   */
-                   
-                   StockTrade filledStockTrade = new StockTrade(arrOfStr);
-                   
-                   if (!filledStockTrade.isValidStockSymbol()) {
-                       System.out.println(filledStockTrade.toString());
-                       System.out.println("ERROR: Invalid stock symbol format:"
-                               + " " + filledStockTrade.getStockSymbol());
-                       System.out.println();
-                       hasStockErrors = true;
-                   }
-                   if (!filledStockTrade.isValidPrice()) {
-                       System.out.println(filledStockTrade.toString());
-                       System.out.println("ERROR: Invalid stock price: " + 
-                               filledStockTrade.getPricePerShare());
-                       System.out.println();
-                       hasStockErrors = true;
-                   }
-                   if (!filledStockTrade.isValidWholeShares()) {
-                       System.out.println(filledStockTrade.toString());
-                       System.out.println("ERROR: Invalid number of shares: " 
-                               + filledStockTrade.getWholeShares());
-                       System.out.println();
-                       hasStockErrors = true;
-                   }
-                   
-                   if (!hasStockErrors) {
-                       displayStockTradeAttributes(filledStockTrade);
-                   }
-               }
-           }
-           System.out.println(brokerLogImpl.getBrokerLog());
+            while(scnr.hasNextLine()) {
+                //boolean hasBrokerErrors = false;
+                //boolean hasStockErrors = false;
+                String str = scnr.nextLine();
+                String[] arrOfStr = str.split(",", 7);
+                if (arrOfStr[0].equals("BROKER")) {
+                    if (arrOfStr[1].equals("ADD")) {
+                        System.out.println("ADDING BROKER");
+                        addBroker(arrOfStr);
+                        
+                    }
+                    if (arrOfStr[1].equals("DEL")) {
+                        System.out.println("DELETING BROKER");
+                        //boolean brokerRemoved = brokerLogImpl.removeBroker(arrOfStr[2]);
+                    }
+                }
+                if (arrOfStr[0].equals("TRADE")) {
+                    if (arrOfStr[1].equals("BUY")) {
+                        System.out.println("BUYING STOCK");
+                    }
+                    if (arrOfStr[1].equals("SELL")) {
+                        System.out.println("SELLING STOCK");
+                    }
+                }  
+               
+            }
         }
         
         catch (IOException error) {
@@ -201,89 +104,106 @@ public class CS310Zimmerman {
             System.exit(1);
         }
         
+        
     }
     
+    public static void addBroker(String[] line) {
+        //BrokerLogImpl brokerLogImpl = new BrokerLogImpl();
+        Broker filledBroker = new Broker(line);
+                       
+        if (!filledBroker.isValidLicense()) {
+            System.out.println(filledBroker.toString());
+            System.out.println("ERROR: Invalid broker license number"
+                + " format: " + filledBroker.getBrokerLicense());
+            System.out.println();
+            //hasBrokerErrors = true;
+        }
+        if (!filledBroker.isValidDept()) {
+            System.out.println(filledBroker.toString());
+            System.out.println("ERROR: Invalid department number"
+                + " format: " + filledBroker.getDept());
+            System.out.println();
+            //hasBrokerErrors = true;
+        }
+        //if hasBrokerErrors is true, do the error message
+        if (brokerLogImpl.isLicenseUnique(line[2])) {
+            if (brokerLogImpl.addBroker(filledBroker)) {
+                System.out.println("ADDED: Broker with license " + line[2]);
+            }
+            else {
+                System.out.println("/tERROR: Broker with license " + line[2] +
+                        "is not unique and will NOT be added to the log");
+            }
+            
+        }
+        
+    }
 
-    
-    /**
-     * 
-     * @param broker - passes in broker object as parameter
-     * @param brokerAttributes - passes in array of string values as parameter
-     * @return - returns the broker object with attributes set
-     * 
-     * Method using setters from Broker class to give empty broker object in 
-     * main values from csv file.
-     */
-    /*
-    public static Broker setBrokerAttributes(Broker broker, 
-            String[] brokerAttributes) {
-        broker.setBrokerLicense(brokerAttributes[2]);
-        broker.setFirstName(brokerAttributes[3]);
-        broker.setLastName(brokerAttributes[4]);
-        broker.setDept(brokerAttributes[5]);
-        broker.setCommissionRate(Double.parseDouble(brokerAttributes[6]));
+    public static void addStockTrade(String[] line) {
+        StockTradeLogImpl stockTradeLog = new StockTradeLogImpl();
         
-    return broker;
+        StockTrade filledStockTrade = new StockTrade(line);
+
+            if (!filledStockTrade.isValidStockSymbol()) {
+                System.out.println(filledStockTrade.toString());
+                System.out.println("ERROR: Invalid stock symbol format:"
+                        + " " + filledStockTrade.getStockSymbol());
+                System.out.println();
+                //hasStockErrors = true;
+            }
+            if (!filledStockTrade.isValidPrice()) {
+                System.out.println(filledStockTrade.toString());
+                System.out.println("ERROR: Invalid stock price: "
+                        + filledStockTrade.getPricePerShare());
+                System.out.println();
+                //hasStockErrors = true;
+            }
+            if (!filledStockTrade.isValidWholeShares()) {
+                System.out.println(filledStockTrade.toString());
+                System.out.println("ERROR: Invalid number of shares: "
+                        + filledStockTrade.getWholeShares());
+                System.out.println();
+                //hasStockErrors = true;
+            }
+            BrokerLogImpl brokerLogImpl = new BrokerLogImpl();
+        
+            String license = line[5];
+            boolean brokerIsUnique = brokerLogImpl.isLicenseUnique(license);
+            boolean stockSymbolIsUnique = stockTradeLog.isStockSymbolUnique(line[2]);
+            if (!brokerIsUnique && stockSymbolIsUnique) {
+                stockTradeLog.addStockTrade(filledStockTrade);
+                System.out.println("ADDED: StockTrade with Stock symbol " + 
+                        line[2] + " listed by Broker " + line[5]);
+            }
+            else if (brokerIsUnique && stockSymbolIsUnique) {
+                System.out.println("ADD ERROR: StockTrade with Stock Symbol " + 
+                        line[2] + "has Broker with license " + line[5]
+                        + " , but there is no such Broker license in the Broker"
+                        + " log. StockTrade " + line[2] + " will NOT be added to "
+                        + "StockTrade log.");
+            }
+            else {
+                System.out.println("ADD ERROR: StockTrade has Stock Symbol "
+                        + "that already exists: " + line[2] + "It will NOT "
+                        + "be added to StockTrade log.");
+            }
+            //might need one more else if for potential of both to be wrong
+            
+
+            
+            
+        
     }
-    */
     
-    /**
-     *
-     * @param broker - passes in broker object as parameter
-     * 
-     * Method that uses getters from Broker class to display the attributes of 
-     * a valid broker object one line at a time
-     */
-    public static void displayBrokerAttributes(Broker broker) {
-        System.out.println(broker.getBrokerLicense());
-        System.out.println(broker.getFirstName());
-        System.out.println(broker.getLastName());
-        System.out.println(broker.getDept());
-        System.out.println(broker.getCommissionRate());
-        System.out.println();
-    }
     
-    /**
-     *
-     * @param stockTrade - passes in stockTrade object as a parameter
-     * @param stockAttributes - passes in array of string values as parameter
-     * @return - returns stockTrade object with attributes set
-     * 
-     * Method using setters from the StockTrade class to give empty stockTrade
-     * object values from csv file.
-     */
-    /*
-    public static StockTrade setStockTradeAttributes(StockTrade stockTrade, 
-            String[] stockAttributes) {
-        stockTrade.setStockSymbol(stockAttributes[2]);
-        stockTrade.setPricePerShare(Double.parseDouble(stockAttributes[3]));
-        stockTrade.setWholeShares(Integer.parseInt(stockAttributes[4]));
-        stockTrade.setBrokerLicense(stockAttributes[5]);
-        if(stockAttributes[6].equals("N")) {
-            stockTrade.setTaxable(false);
-        }
-        if(stockAttributes[6].equals("Y")) {
-            stockTrade.setTaxable(true);
+    
+    public static void deleteBroker(String[] line) {
+        BrokerLogImpl brokerLogImpl = new BrokerLogImpl();
+        
+        String license = line[2];
+        if (!brokerLogImpl.isLicenseUnique(license)) {
+           brokerLogImpl.removeBroker(license); 
         }
         
-    return stockTrade;
     }
-    */
-    
-    /**
-     *
-     * @param stockTrade - passes in stockTrade object as parameter
-     * 
-     * Method that uses getters from StockTrade class to display attributes of
-     * valid stockTrade objects one line at a time.
-     */
-    public static void displayStockTradeAttributes(StockTrade stockTrade) {
-        System.out.println(stockTrade.getStockSymbol());
-        System.out.println(stockTrade.getPricePerShare());
-        System.out.println(stockTrade.getWholeShares());
-        System.out.println(stockTrade.getBrokerLicense());
-        System.out.println(stockTrade.isTaxable());
-        System.out.println();
-    }
-    
 }
