24fd13798d912cb41a9eb1b9d65e0613a6c1d4f2
cobyz <cobyz@192.168.1.27>
2019-09-29 11:14:56 -0700

    Broker Impl Class

---
+++ src/cs310zimmerman/BrokerLogImpl.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs310zimmerman;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author cobyz
+ */
+public class BrokerLogImpl {
+    private ArrayList<Broker> brokerLog = new ArrayList<Broker>();
+    
+    public ArrayList getBrokerLog() {
+        return brokerLog;
+    }
+    
+    public boolean addBroker(Broker brokerObj) {
+       boolean successful = true;
+       //boolean brokerAdded = false;
+       int arraySize = brokerLog.size() - 1;
+       
+       
+       if (!brokerLog.isEmpty()) {
+           for (int i = 0; i <= arraySize; i++) {
+               //if(!brokerAdded) {
+                   Broker brokerLogObj = brokerLog.get(i);
+               
+                   System.out.println("BrokerObj: " + brokerObj.getBrokerLicense());
+                   int x = brokerLogObj.getBrokerLicense().compareTo(brokerObj.getBrokerLicense());
+                   if (x < 0) {
+                       System.out.println("I'm bigger than current Broker" + brokerLogObj.getBrokerLicense());
+                   }
+                   else {
+                       brokerLog.add(i, brokerObj);
+                       //brokerAdded = true;
+                   }
+               //}
+               
+           }
+           brokerLog.add(brokerObj);
+           //brokerAdded = true;
+           return successful;
+       }
+       
+       else {
+           brokerLog.add(brokerObj);
+           System.out.println("in else");
+       }
+        //System.out.println(brokerLog);
+  
+    return successful; 
+    }
+    
+    public boolean removeBroker(String license) {
+        boolean successful = false;
+        int arraySize = brokerLog.size() - 1;
+        
+        for (int i = 0; i < arraySize; i++) {
+            Broker brokerLogObj = brokerLog.get(i);
+            if (brokerLogObj.getBrokerLicense().equals(license)) {
+                brokerLog.remove(i);
+                successful = true;
+            }
+        }
+    return successful;
+    }
+    
+    public boolean isLicenseUnique(String license) {
+        boolean isUnique = true;
+        int arraySize = brokerLog.size() - 1;
+        
+        for (int i = 0; i < arraySize; i++) {
+            Broker brokerLogObj = brokerLog.get(i);
+            isUnique = !brokerLogObj.getBrokerLicense().equals(license);
+        }
+    return isUnique;
+    }
+}
