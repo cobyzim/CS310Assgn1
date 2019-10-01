8bbca02de927c408b4c06ab365e9c925f85b0c90
cobyz <cobyz@192.168.0.19>
2019-09-30 23:53:32 -0700

    added nested loops

--- src/cs310zimmerman/PrintImpl.java
+++ src/cs310zimmerman/PrintImpl.java
@@ -18,11 +18,25 @@ public class PrintImpl {
     private  int numStockTrades;
     
     public void printReport(BrokerLogImpl brokerLogImpl, StockTradeLogImpl stockTradeLogImpl) {
+        final String OUTPUT_FILENAME = "output/assn2report.txt";
         brokerLog = brokerLogImpl.getBrokerLog();
         stockTradeArray = stockTradeLogImpl.getStockTradeArray();
         numStockTrades = stockTradeLogImpl.getNumStockTrades();
+        int arraySize = brokerLog.size() - 1;
+        String brokerLicense;
+        String tradeLicense;
         
-        for (int i = 0; i < numStockTrades; i++) {
+        for (int i = 0; i <= arraySize; i++) {
+            Broker brokerLogObj = brokerLog.get(i);
+            brokerLicense = brokerLogObj.getBrokerLicense();
+            
+            for (int j = 0; j < numStockTrades; i++) {
+                StockTrade stockTradeObj = stockTradeArray[i];
+                tradeLicense = stockTradeObj.getBrokerLicense();
+                if (brokerLicense.equals(tradeLicense)) {
+                    
+                }
+            }
             
         }
         
