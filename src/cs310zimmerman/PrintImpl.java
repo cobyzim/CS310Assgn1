67d5db34b02e3c59f3cb38fc157b38007dbdda02
cobyz <cobyz@192.168.0.19>
2019-10-01 09:30:50 -0700

    added to the nested loops

--- src/cs310zimmerman/PrintImpl.java
+++ src/cs310zimmerman/PrintImpl.java
@@ -17,23 +17,48 @@ public class PrintImpl {
     private StockTrade[] stockTradeArray;
     private  int numStockTrades;
     
-    public void printReport(BrokerLogImpl brokerLogImpl, StockTradeLogImpl stockTradeLogImpl) {
+    public void printReport(BrokerLogImpl brokerLogImpl, StockTradeLogImpl stockTradeLogImpl) throws IOException {
         final String OUTPUT_FILENAME = "output/assn2report.txt";
         brokerLog = brokerLogImpl.getBrokerLog();
         stockTradeArray = stockTradeLogImpl.getStockTradeArray();
         numStockTrades = stockTradeLogImpl.getNumStockTrades();
         int arraySize = brokerLog.size() - 1;
         String brokerLicense;
+        String firstName;
+        String lastName;
+        String dept;
+        double commissionRate;
         String tradeLicense;
+        String stockSymbol;
+        double pricePerShare;
+        int wholeShares;
+        boolean taxable;
+        
         
         for (int i = 0; i <= arraySize; i++) {
             Broker brokerLogObj = brokerLog.get(i);
             brokerLicense = brokerLogObj.getBrokerLicense();
+            firstName = brokerLogObj.getFirstName();
+            lastName = brokerLogObj.getLastName();
+            dept = brokerLogObj.getDept();
+            commissionRate = brokerLogObj.getCommissionRate();
             
             for (int j = 0; j < numStockTrades; i++) {
                 StockTrade stockTradeObj = stockTradeArray[i];
                 tradeLicense = stockTradeObj.getBrokerLicense();
+                stockSymbol = stockTradeObj.getStockSymbol();
+                pricePerShare = stockTradeObj.getPricePerShare();
+                wholeShares = stockTradeObj.getWholeShares();
+                if (stockTradeObj.isTaxable() == true) {
+                    taxable = "YES";
+                }
                 if (brokerLicense.equals(tradeLicense)) {
+                    FileWriter fileWriter = new FileWriter(OUTPUT_FILENAME);
+                    PrintWriter printWriter = new PrintWriter(fileWriter);
+                    
+                    printWriter.printf("%s  %s,  %s\n", brokerLicense, 
+                        brokerLogObj.getLastName(), brokerLogObj.getFirstName());
+                    printWriter.printf("\n\t%s \t.2f \t%d   %s", )
                     
                 }
             }
@@ -43,5 +68,4 @@ public class PrintImpl {
         
         
     }
-    //loop through brokers, grab license, loop through stocktrades, compare if they have same license
 }
