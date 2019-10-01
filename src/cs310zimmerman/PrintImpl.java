8fa396a8727ca2865f21b0ac88785d249ffb18f7
cobyz <cobyz@192.168.0.19>
2019-09-30 22:46:37 -0700

    created print impl class

---
+++ src/cs310zimmerman/PrintImpl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs310zimmerman;
+
+import java.util.ArrayList;
+import java.io.*;
+/**
+ *
+ * @author cobyz
+ */
+public class PrintImpl {
+    private ArrayList<Broker> brokerLog;
+    private String[] stockTradeArray;
+    private  int numStockTrades;
+    
+    //public static void printReport()
+    //loop through brokers, grab license, loop through stocktrades, compare if they have same license
+}
