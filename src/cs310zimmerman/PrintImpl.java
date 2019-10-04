ba4f0a46ec29f270f43273db49a3032e9cd9023c
cobyz <cobyz@192.168.0.19>
2019-10-03 20:34:32 -0700

    changed print impl loops

--- src/cs310zimmerman/PrintImpl.java
+++ src/cs310zimmerman/PrintImpl.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Implementation Class containing a printReport method to generate a report
+ * that shows the broker objects that have stocktrades. It also shows the number
+ * of stock trade listings for each broker, the total value of the stock trade
+ * listings for each broker, and both of the latter for all of the brokers.
  */
 package cs310zimmerman;
 
@@ -17,6 +18,15 @@ public class PrintImpl {
     private StockTrade[] stockTradeArray;
     private  int numStockTrades;
     
+    /**
+     * Method used to generate the report using the broker and stock trade
+     * implementation objects.
+     * 
+     * @param brokerLogImpl - passes in the broker implementation object
+     * @param stockTradeLogImpl - passes in stock trade implementation object
+     * @throws IOException - throws exception if I/O operations fail or are
+     * interrupted
+     */
     public void printReport(BrokerLogImpl brokerLogImpl, StockTradeLogImpl stockTradeLogImpl) throws IOException {
         final String OUTPUT_FILENAME = "output/assn2report.txt";
         brokerLog = brokerLogImpl.getBrokerLog();
@@ -38,6 +48,7 @@ public class PrintImpl {
         double totalListValue;
         int numListingsAll = 0;
         double totalListValueAll = 0.0;
+        int numStockPerBroker = 0;
         
         FileWriter fileWriter = new FileWriter(OUTPUT_FILENAME);
         PrintWriter printWriter = new PrintWriter(fileWriter);
@@ -68,8 +79,7 @@ public class PrintImpl {
                 }
                 numListings = numStockTrades;
                 totalListValue = stockTradeLogImpl.totalStockTradeValue(brokerLicense);
-                
-                
+                numStockPerBroker = stockTradeLogImpl.numberOfBrokerStockTrades(tradeLicense);
                 
                 if (brokerLicense.equals(tradeLicense)) {
                     System.out.println("In if license compare check");
@@ -77,8 +87,10 @@ public class PrintImpl {
                     printWriter.printf("%s  %s,  %s\n", brokerLicense, 
                         lastName, firstName);
                     
-                    printWriter.printf("\n\t%s \t%.2f \t%d   %s\n", stockSymbol, 
+                    for (int k = 0; k < numStockPerBroker; k++) {
+                        printWriter.printf("\n\t%s \t%.2f \t%d   %s\n", stockSymbol, 
                             pricePerShare, wholeShares, taxable);
+                    }
                     
                     printWriter.printf("\n   Number of StockTrade Listings for "
                             + "Broker: %d\n", numListings);
