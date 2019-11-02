33dbd588e210a8b27156b68e4f3ed9020ac94170
cobyz <cobyz@192.168.1.13>
2019-11-02 15:54:32 -0700

    Created BrokerQueueImpl

---
+++ src/cs310zimmerman/BrokerQueueImpl.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs310zimmerman;
+
+/**
+ * 
+ * @author 
+ * @version
+ */
+public class BrokerQueueImpl 
+{
+    private BrokerNode first;   // reference to the first item in the queue
+    private BrokerNode last;    // reference to the last item in the queue
+
+    /**
+     *
+     */
+    public BrokerQueueImpl() 
+    {
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isEmpty() 
+    {
+        
+        return true;    // update this and remove this comment
+    }
+
+    /**
+     *
+     * @param broker
+     */
+    public void add(Broker broker) 
+    {
+        
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Broker remove() 
+    {
+        
+        return null;    // update this and remove this comment
+    }
+    
+}
