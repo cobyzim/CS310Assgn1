ef0bfec0296468dcac909602c2069c84bd2b8c8c
cobyz <cobyz@192.168.0.19>
2019-11-18 09:37:27 -0800

    javadoc

--- src/cs310zimmerman/StockTradeNode.java
+++ src/cs310zimmerman/StockTradeNode.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Class used to set and retrieve values for stockTrade object nodes as well as
+ * the next node each node references.
  */
 package cs310zimmerman;
 
@@ -13,23 +12,48 @@ public class StockTradeNode {
     private StockTradeNode next;
     private StockTrade data;
     
+    /**
+     * Method used to create a stockTradeNode using a stockTrade object
+     * 
+     * @param stockTrade
+     */
     public StockTradeNode(StockTrade stockTrade) {
         next = null;
         data = stockTrade;
     }
     
+    /**
+     * Method used to retrieve the next node in the list
+     * 
+     * @return - returns the next node
+     */
     public StockTradeNode getNext() {
         return next;
     }
     
+    /**
+     * Method used to set a stockTrade's next node to another node
+     * 
+     * @param stockTradeNode - passes in stockTradeNode
+     */
     public void setNext(StockTradeNode stockTradeNode) {
         next = stockTradeNode;
     }
     
+    /**
+     * Method that gives a node the value of a stockTrade object
+     * 
+     * @param stockTrade
+     */
     public void setData(StockTrade stockTrade) {
         data = stockTrade;
     }
     
+    /**
+     * Method that retrieves data from a given node
+     * 
+     * @return - returns the data
+     */
     public StockTrade getData() {
         return data;
     }
