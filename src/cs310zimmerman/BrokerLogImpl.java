680d02d69a3acc20c06c28894cecdef7853ef750
cobyz <cobyz@192.168.0.19>
2019-11-30 22:31:59 -0800

    started adding binary tree search stuff

--- src/cs310zimmerman/BrokerLogImpl.java
+++ src/cs310zimmerman/BrokerLogImpl.java
@@ -4,24 +4,74 @@
  */
 package cs310zimmerman;
 
+
+import java.io.*;
 /**
  *
  * @author Coby Zimmerman
  */
 public class BrokerLogImpl {
+    
+    protected static class BrokeNode {
+        protected Broker data;
+        protected BrokeNode left;
+        protected BrokeNode right;
+        
+        public BrokeNode(Broker data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    
+        public String toString() {
+            return data.toString();
+        }
+   
+    }
 
-    private Broker[] brokerHashTable;
-    private int numBrokersInTable;
-    private final int BROKER_TABLE_SIZE = 19;
+    protected BrokeNode root;
+    protected boolean addReturn; //I think this is right but im not sure
     
     /**
      * Constructor that initializes the hashTable size and provides a counter
      */
     public BrokerLogImpl() {
-        brokerHashTable = new Broker[BROKER_TABLE_SIZE];
-        numBrokersInTable = 0;
+        
+        root = null;
+        
+    }
+    
+    protected BrokerLogImpl(BrokeNode root) {
+        
+        this.root = root;
+        
+    }
+    
+    public BrokerLogImpl(Broker data, BrokerLogImpl leftTree, BrokerLogImpl rightTree) {
+        
+        root = new BrokeNode(data);
+        
+        if (leftTree != null) {
+            root.left = leftTree.root;
+        }
+        else {
+            root.left = null;
+        }
+        
+        if (rightTree != null) {
+            root.right = rightTree.root;
+        }
+        else {
+            root.right = null;
+        }
+        
     }
 
+    public boolean addBroker(Broker broker) {
+        root = addBroker(root, broker);
+        return addReturn;
+    }
+    
     /**
      * Method used to add brokers to hashSet of brokers if there is room and
      * the brokerLicense is unique
@@ -30,7 +80,47 @@ public class BrokerLogImpl {
      * @return - returns true or false based on if a broker object is added or 
      * not
      */
-    public boolean addBroker(Broker brokerObj) {
+    
+    private BrokeNode addBroker(BrokeNode localRoot, Broker broker) {
+        
+        
+        //if tree empty, return new node
+        if (localRoot == null) {
+            
+            //item not in tree - insert it
+            addReturn = true;
+            System.out.printf("Added broker with license %s\n", broker.getBrokerLicense());
+            
+            return new BrokeNode(broker);
+        }
+        
+        else if (broker.getBrokerLicense().compareTo(localRoot.data.getBrokerLicense()) == 0) {
+            
+            //target license is equal to localRoot brokerLicense - do not add it
+            addReturn = false;
+            
+            return localRoot;
+        }
+        
+        else if (broker.getBrokerLicense().compareTo(localRoot.data.getBrokerLicense()) < 0) {
+            
+            //target license is less than localRoot brokerLicense - add it
+            localRoot.left = addBroker(localRoot.left, broker);
+            System.out.printf("Added broker with license %s to left subtree\n", broker.getBrokerLicense());
+            
+            return localRoot;
+        }
+        
+        else {
+            
+            //item is greater than localRoot brokerLicense - add it
+            localRoot.right = addBroker(localRoot.right, broker);
+            System.out.printf("Added broker with license %s to right subtree\n", broker.getBrokerLicense());
+            
+            return localRoot;
+        }
+        
+        /*
         boolean isAdded = false;
 
         int hashValue = brokerObj.hashCode();
@@ -64,6 +154,8 @@ public class BrokerLogImpl {
         }      
         return isAdded;
     }
+    */
+    }
     
     /**
      * Method that finds a broker if it is in the hashSet given a certain 
@@ -73,8 +165,28 @@ public class BrokerLogImpl {
      * @return - returns reference to the broker object or null if it is not in 
      * the hashSet
      */
-    public Broker findBroker(String brokerLicense) {
+    public Broker findBroker(Broker target) {
+        return findBroker(root, target);
+    }
+    
+    private Broker findBroker(BrokeNode localRoot, Broker target) {
+        if (localRoot == null) {
+            return null;
+        }
         
+        //Below compares target with data field at the root
+        int compResult = target.getBrokerLicense().compareTo(localRoot.data.getBrokerLicense());
+        if (compResult == 0) {
+            return localRoot.data;
+        }
+        else if (compResult < 0) {
+            return findBroker(localRoot.left, target);
+        }
+        else {
+            return findBroker(localRoot.right, target);
+        }
+        
+        /*
         Broker dummyBroker = new Broker();
         dummyBroker.setBrokerLicense(brokerLicense);
         int hashCode = dummyBroker.hashCode();
@@ -87,27 +199,10 @@ public class BrokerLogImpl {
         }
         else {
             return null;
-        }       
-    }
-
-    /**
-     * Method that displays the contents of the broker hashTable
-     * 
-     */
-    public void displayHash() {
-        System.out.println("Broker Hash Table:");
-        
-        for (int i = 0; i < BROKER_TABLE_SIZE; i++) {
-            if (brokerHashTable[i] == null) {
-                System.out.printf("\tIndex %d is empty\n", i);
-            }
-            else {
-                System.out.printf("\tIndex %d contains Broker %s, %s %s\n", i, 
-                    brokerHashTable[i].getBrokerLicense(), 
-                    brokerHashTable[i].getFirstName(), 
-                    brokerHashTable[i].getLastName());
-            }
         }
+        */
+        
     }
+    
 }
 
